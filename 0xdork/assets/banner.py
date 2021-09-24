4238e207bfc26909bd104eda24a6b009cd97a254
Richard Mwewa <74001397+rlyonheart@users.noreply.github.com>
2021-09-24 13:57:50 +0200

    Create banner.py

---
+++ 0xdork/assets/banner.py
@@ -0,0 +1,12 @@
+from assets.colors import red,white,end
+banner = f"""{red}
+      ▐▄• ▄ ·▄▄▄▄        ▄▄▄  ▄ •▄ 
+▪      █▌█▌▪██▪ ██ ▪     ▀▄ █·█▌▄▌▪
+ ▄█▀▄  ·██· ▐█· ▐█▌ ▄█▀▄ ▐▀▀▄ ▐▀▀▄·
+▐█▌.▐▌▪▐█·█▌██. ██ ▐█▌.▐▌▐█•█▌▐█.█▌
+ ▀█▄▀▪•▀▀ ▀▀▀▀▀▀▀•  ▀█▄▀▪.▀  ▀·▀  ▀
+Disclaimer{white}:
+This tool was developed for educational purposes and should not be used in environments
+without {red}Legal Authorization{white}.
+Therefore, the author shall {red}NOT{white} be responsible for the damages that might be done with it.{end}
+"""
