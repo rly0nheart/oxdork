622ebdbd8b606f2af1d439acb5bc2d9790bd9a55
Richard Mwewa <74001397+rlyonheart@users.noreply.github.com>
2021-10-03 00:49:20 +0200

    Update banner.py

--- 0xdork/assets/banner.py
+++ 0xdork/assets/banner.py
@@ -1,11 +1,11 @@
-from assets.colors import red,white,end
+from assets.colors import red,end,white
 banner = f"""{red}
-      ▐▄• ▄ ·▄▄▄▄        ▄▄▄  ▄ •▄ 
-▪      █▌█▌▪██▪ ██ ▪     ▀▄ █·█▌▄▌▪
- ▄█▀▄  ·██· ▐█· ▐█▌ ▄█▀▄ ▐▀▀▄ ▐▀▀▄·
-▐█▌.▐▌▪▐█·█▌██. ██ ▐█▌.▐▌▐█•█▌▐█.█▌
- ▀█▄▀▪•▀▀ ▀▀▀▀▀▀▀•  ▀█▄▀▪.▀  ▀·▀  ▀
-Disclaimer{white}:
+╭━━━╮╱╱╭╮╱╱╱╭╮
+┃╭━╮┣┳┳╯┣━┳┳┫┣╮
+┃┃┃┃┣┃┫╋┃╋┃╭┫━┫
+┃┃┃┃┣┻┻━┻━┻╯╰┻╯
+┃╰━╯┃
+ Disclaimer{white}:
 This tool was developed for educational purposes and should not be used in environments
 without {red}Legal Authorization{white}.
 Therefore, the author shall {red}NOT{white} be responsible for the damages that might be done with it.{end}
