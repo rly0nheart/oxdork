d17ef7d207743f06f58d44713a0fb2ca785a6949
Richard Mwewa <74001397+rlyonheart@users.noreply.github.com>
2021-10-09 19:01:25 +0200

    Create banner.py

---
+++ oxdork/lib/banner.py
@@ -0,0 +1,12 @@
+from lib.colors import red,end,white
+banner = f"""{red}
+╭━━━╮╱╱╭╮╱╱╱╭╮
+┃╭━╮┣┳┳╯┣━┳┳┫┣╮
+┃┃┃┃┣┃┫╋┃╋┃╭┫━┫
+┃┃┃┃┣┻┻━┻━┻╯╰┻╯
+┃╰━╯┃
+Disclaimer{white}:
+This tool was developed for educational purposes and should not be used in environments
+without {red}legal authorization{white}.
+Therefore, the author shall {red}not{white} be responsible for the damages that might be done with it.{end}
+"""
