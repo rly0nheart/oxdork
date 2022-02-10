2cb13cfa0bba3732734898248d48fc7f7f91daec
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2022-02-10 10:25:57 +0200

    Delete .__main__.sh

--- src/.__main__.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# This is a bash version of --lite 
-# It uses curl to send requests
-
-query="https://www.google.com/search?q=$1"
-curl "https://api.hackertarget.com/pagelinks/?q=$query"
