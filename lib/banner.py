64db827e00a5272057d48e34bd17106ac7b178ac
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2022-02-10 10:23:52 +0200

    Update and rename headers.py to banner.py

---
+++ lib/banner.py
@@ -0,0 +1,10 @@
+from lib.colors import red,white,reset
+banner = f"""{red}
+▐▄• ▄ ·▄▄▄▄        ▄▄▄  ▄ •▄ 
+▪      █▌█▌▪██▪ ██ ▪     ▀▄ █·█▌▄▌▪
+ ▄█▀▄  ·██· ▐█· ▐█▌ ▄█▀▄ ▐▀▀▄ ▐▀▀▄·
+▐█▌.▐▌▪▐█·█▌██. ██ ▐█▌.▐▌▐█•█▌▐█.█▌
+ ▀█▄▀▪•▀▀ ▀▀▀▀▀▀▀•  ▀█▄▀▪.▀  ▀·▀  ▀
+Disclaimer{white}:
+This tool was developed for educational purposes and should not be used in environments without legal authorization.{reset}
+"""
