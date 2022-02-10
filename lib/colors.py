40d7ad3e100cbbeac420222490c7f29552267c29
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2022-02-10 10:25:29 +0200

    Update colors.py

--- lib/colors.py
+++ lib/colors.py
@@ -5,10 +5,9 @@ machine = sys.platform # Detecting the os of current system
 if machine.lower().startswith(('os', 'win', 'darwin', 'ios')):
     colors = False # Colors will not be displayed in mac & windows
 if not colors:
-    end = red = white = green = yellow = ''
+    red = white = green = reset = ""
 else:
     white = "\x1b[97m"
     green = "\x1b[92m"
     red = "\x1b[91m"
-    end = "\x1b[0m"
-    yellow = "\x1b[93m"
+    reset = "\x1b[0m"
