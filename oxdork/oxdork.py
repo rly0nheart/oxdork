27823781c2fc287d8bc177b160b81978e1d019e8
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2023-06-26 06:22:52 +0200

    Update oxdork.py

--- oxdork/oxdork.py
+++ oxdork/oxdork.py
@@ -1,41 +1,51 @@
 import os
-import time
-import requests
-from datetime import datetime
-from rich.prompt import Prompt
+import json
 from googlesearch import search
 from rich import print as xprint
-from oxdork.banner import ascii_banner
-from oxdork.config import log, arguments
+from urllib.request import urlopen
+from oxdork.config import log, Version
+
+version = Version()
 
 
-# Process user input
-# If input is a file, open it and loop through each line and use it as a query.
-# in the start_search_with_query function
-# If input is a string, call start_search_with_query
-def process_user_query(query):
-    if os.path.isfile(query):
-        with open(query, 'r') as file:
+def process_input(user_input) -> list:
+    """
+    Processes user input. If input is a file, opens the file and reads the contents, line by line.
+    If not, use the input as it is
+
+    :param user_input: user input
+    :return: A list of the processed queries/query
+    """
+    if os.path.isfile(user_input):
+        with open(user_input, 'r') as file:
             log.info(f"Loaded queries from file: {file.name}")
-            for count, line in enumerate(file, start=1):
-                log.info(f"Current query: {count}. {line}")
-                start_search_with_query(query=line, count=arguments.count, output=arguments.output)
+            queries = file.readlines()
+            queries = [query.strip() for query in queries]
+            return queries
     else:
-        start_search_with_query(query=query, count=arguments.count, output=arguments.output)
+        return [user_input]
 
 
-# Start search with the query from the process_query function
-def start_search_with_query(query, count, output):
+def begin_search(query, count, output) -> None:
+    """
+    Start search with the processed input
+
+    :param query: Search query
+    :param count: Number of results to return (defaults to 10)
+    :param output: String representing a file to which results will be written
+    :return: None
+    """
     number = 0
-    log.info(f"Fetching {count} results for {query}...")
-    for counter, result in enumerate(search(query, num=int(count), start=0, stop=None, lang="en", tld="com", pause=2.5), start=1):
+    log.info(f"Fetching {count} results for `{query}`...")
+    for counter, result in enumerate(search(query, num=int(count), start=0, stop=None,
+                                            lang="en", tld="com", pause=2.5), start=1):
         number += 1
         log.info(f"{counter}. {result}")
 
-        # Start writting results to a file
+        # Start writing results to a file
         # if user passes the filename to -o/--output
         if output:
-            write_output(result, counter, output)
+            __write_output(result, counter, output)
 
         # If result number is greate than or equal to
         # the user specified count limit. Break the search loop. 
@@ -43,19 +53,35 @@ def start_search_with_query(query, count, output):
             break
 
 
-# Write results to a file
-def write_output(result, counter, output):
-    with open(f"{output}.txt", "a") as file:
+def __write_output(result: str, counter: int, filename: str) -> None:
+    """
+    Write a result to a file.
+
+    :param result: The result to write.
+    :type result: str
+    :param counter: The index of the result.
+    :type counter: int
+    :param filename: The name of the output file (without extension).
+    :type filename: str
+    :return: None
+    """
+    with open(f"{filename}.txt", "a") as file:
         file.write(f"{counter}. {result}\n")
-        file.close()
 
 
-# Check program updates
-def check_updates():
-    response = requests.get("https://api.github.com/repos/rly0nheart/oxdork/releases/latest").json()
-    if response['tag_name'] == ascii_banner()[1]:
-        pass
-    else:
-        log.info(f"A new release is available: oxDork {response['tag_name']}")
-        xprint(f"\n{response['body']}\n")
-        log.info("Run 'pip install --upgrade oxdork' to get the updates.\n")
+def check_updates() -> None:
+    """
+    Checks for latest updates by retrieving the release tag from the releases page of the program from GitHub
+    Then compares the remote version tag with the tag in the program.
+    If they match, program assumes it's up-to-date.
+    If not, print a message notifying the user about the remote version (which is treated as the official new release)
+    , and lastly prints the release notes of the presumed new release.
+
+    :return: None
+    """
+    with urlopen("https://api.github.com/repos/rly0nheart/oxdork/releases/latest") as response:
+        release_data = json.loads(response.read().decode())
+        if release_data['tag_name'] != version.full_version():
+            xprint(f"* A new release of `OxDork` is available ({release_data['tag_name']}).\n")
+        else:
+            pass
